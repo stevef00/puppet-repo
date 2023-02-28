498701836ef02271c197bf647aa22b91d0253af1
Steve Feehan <stevef00@pobox.com>
2023-02-28 13:55:41 -0500

    profile::base::auth: sudo env_keep += SSH_AUTH_SOCK

--- modules/profile/manifests/base/auth.pp
+++ modules/profile/manifests/base/auth.pp
@@ -2,6 +2,13 @@
 class profile::base::auth {
   class { 'authselect': policy => 'custom/local' }
 
-  package { 'pam_ssh_agent_auth': ensure => installed }
+  file { '/etc/sudoers.d/ssh_auth_sock':
+    owner   => root,
+    group   => root,
+    mode    => '0600',
+    content => 'Defaults env_keep += SSH_AUTH_SOCK'
+  }
+  -> package { 'pam_ssh_agent_auth': ensure => installed }
   -> authselect::feature { 'with-pam_ssh_agent_auth': enable => true }
+
 }
