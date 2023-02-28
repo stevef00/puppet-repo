1c8010574de8f4cd48a97f8c5f42bdae9af3142d
Steve Feehan <stevef00@pobox.com>
2023-02-28 09:11:50 -0500

    profile::base::auth: try with-pam_ssh_agent_auth

--- modules/profile/manifests/base/auth.pp
+++ modules/profile/manifests/base/auth.pp
@@ -1,4 +1,7 @@
 # base auth configuration
 class profile::base::auth {
   class { 'authselect': policy => 'custom/local' }
+
+  package { 'pam_ssh_agent_auth': ensure => installed }
+  -> authselect::feature { 'with-pam_ssh_agent_auth': enable => true }
 }
