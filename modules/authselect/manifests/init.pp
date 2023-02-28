2ab96f39e4f759ac6fa539ed29f8661cecd3171a
Steve Feehan <stevef00@pobox.com>
2023-02-28 08:15:14 -0500

    authselect: add custom/local policy
    This is a copy of the 'minimal' policy. The implementation
    needs some work. I'm not sure how to manage a custom policy
    cleanly and separate the 'local' code (ie. the custom policy)
    from the base authselect module. Also, the custom policy is
    optional so we need to handle that correctly.

--- modules/authselect/manifests/init.pp
+++ modules/authselect/manifests/init.pp
@@ -1,7 +1,14 @@
 # set and manage authselect policy
 class authselect ($policy='minimal') {
 
-  exec { 'authselect_policy':
+  file { "/etc/authselect/${policy}":
+    ensure  => directory,
+    source  => "puppet:///modules/auth/authselect/${policy}",
+    recurse => true,
+    purge   => true,
+    force   => true,
+  }
+  -> exec { 'authselect_policy':
     command => "authselect select ${policy}",
     unless  => "authselect current | grep -q \"^Profile ID: ${policy}\$\" 2>/dev/null",
     path    => [ '/bin', '/sbin', '/usr/bin', '/usr/sbin' ];
