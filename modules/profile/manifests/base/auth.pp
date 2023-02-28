1e059b2f60e177c95a2351e7c0fcddbf26b02fd9
Steve Feehan <stevef00@pobox.com>
2023-02-28 15:28:29 -0500

    profile::base::auth: sudo want's a newline

--- modules/profile/manifests/base/auth.pp
+++ modules/profile/manifests/base/auth.pp
@@ -6,7 +6,7 @@ class profile::base::auth {
     owner   => root,
     group   => root,
     mode    => '0600',
-    content => 'Defaults env_keep += SSH_AUTH_SOCK'
+    content => "Defaults env_keep += SSH_AUTH_SOCK\n"
   }
   -> package { 'pam_ssh_agent_auth': ensure => installed }
   -> authselect::feature { 'with-pam_ssh_agent_auth': enable => true }
